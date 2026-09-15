b24de37935b9fc9325eb87fceaa11cffde871d89
Bibek Singh <bbek1717ingh@gmail.com>
2026-09-15 19:03:09 +0545

    pod configuration

--- sandbox/server/src/kubernetes/pod.js
+++ sandbox/server/src/kubernetes/pod.js
@@ -1,2 +1,34 @@
 import {k8sCoreV1Api} from "./config.js"
 
+export async function createPod(sandboxId){
+
+    const podManifest = {
+        metadata : {
+            name: `sandbox-pod ${sandboxId}`,
+            labels: {
+                app : 'sandbox', 
+                sandboxId : sandboxId
+            }
+        },
+        spec: {
+            containers: [
+                {
+                    image : "template",
+                    imagePullPolicy: "IfNoPresent",
+                    name : "sandbox-container", 
+                    ports: [{ containerPort: 5173, name: "http"}],
+                    resoures: {
+                        limits: {
+                            cpu : "500m",
+                            memory: "1Gi"
+                        },
+                        requests: {
+                            cpu : "250m",
+                            memory: "500Mi"
+                        }
+                    }
+                }
+            ]
+        }
+    }
+}
